3df6093acfaa9987a58691e8df6f15377847d5b4
floydzhang1984 <tech_universe@163.com>
2014-10-21 15:18:53 +0800

    Test with the training page

--- src/com/example/webviewtest/MainActivity.java
+++ src/com/example/webviewtest/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.webviewtest;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebSettings.RenderPriority;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -10,8 +12,9 @@ public class MainActivity extends Activity {
 
 	//记得一定要前面加上http, 否则会报找不到网页
 	//private String url = "http://119.254.108.211/ftphome/android";
-	private String url = "http://119.254.108.211/ftphome/android";
+	private String url = "http://119.254.108.211/ftphome/android/training/index.html";
 	public static String TAG = "webview";
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,6 +23,8 @@ public class MainActivity extends Activity {
 		webview.getSettings().setJavaScriptEnabled(true); //设置能够执行JS脚本
 		webview.getSettings().setBuiltInZoomControls(true); //设置支持缩放
 		webview.getSettings().setLoadWithOverviewMode(true); //设置适应屏幕大小
+		webview.getSettings().setRenderPriority(RenderPriority.HIGH);
+		//webview.getSettings().setBlockNetworkImage(true);
 		webview.loadUrl(url);
 		webview.setWebViewClient(new WebViewClient() {
 			public boolean shouldOverrideUrlLoading(WebView view, String url) {
